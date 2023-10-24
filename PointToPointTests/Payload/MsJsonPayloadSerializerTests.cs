50c8032d5dcd38fec59fd3b2b4e34dac30ac02d5
Pontus Markström <pontus.markstrom@gmail.com>
2023-10-24 20:42:56 +0200

    Add unittests

---
+++ PointToPointTests/Payload/MsJsonPayloadSerializerTests.cs
@@ -0,0 +1,32 @@
+﻿using PointToPoint.Payload;
+using PointToPoint.Protocol;
+
+namespace PointToPointTests.Payload;
+
+[TestClass]
+public class MsJsonPayloadSerializerTests
+{
+    [TestMethod]
+    public void SerializeDeserialize_CustomMessage()
+    {
+        var message = new MyMessage(10, "text");
+        var serializer = new MsJsonPayloadSerializer(typeof(MyMessage).Namespace!);
+
+        var payload = serializer.MessageToPayload(message);
+        var deserializedMessage = serializer.PayloadToMessage(payload, payload.Length);
+
+        Assert.AreEqual(message, deserializedMessage);
+    }
+
+    [TestMethod]
+    public void SerializeDeserialize_KeepAlive()
+    {
+        var message = new KeepAlive();
+        var serializer = new MsJsonPayloadSerializer(typeof(MyMessage).Namespace!);
+
+        var payload = serializer.MessageToPayload(message);
+        var deserializedMessage = serializer.PayloadToMessage(payload, payload.Length);
+
+        Assert.AreEqual(message, deserializedMessage);
+    }
+}
---
+++ PointToPointTests/Payload/NewtonsoftJsonPayloadSerializerTests.cs
@@ -0,0 +1,34 @@
+﻿using PointToPoint.Payload;
+using PointToPoint.Protocol;
+
+namespace PointToPointTests.Payload;
+
+[TestClass]
+public class NewtonsoftJsonPayloadSerializerTests
+{
+    [TestMethod]
+    public void SerializeDeserialize_CustomMessage()
+    {
+        var message = new MyMessage(10, "text");
+        var serializer = new NewtonsoftJsonPayloadSerializer(typeof(MyMessage).Namespace!);
+
+        var payload = serializer.MessageToPayload(message);
+        var deserializedMessage = serializer.PayloadToMessage(payload, payload.Length);
+
+        Assert.AreEqual(message, deserializedMessage);
+    }
+
+    [TestMethod]
+    public void SerializeDeserialize_KeepAlive()
+    {
+        var message = new KeepAlive();
+        var serializer = new NewtonsoftJsonPayloadSerializer(typeof(MyMessage).Namespace!);
+
+        var payload = serializer.MessageToPayload(message);
+        var deserializedMessage = serializer.PayloadToMessage(payload, payload.Length);
+
+        Assert.AreEqual(message, deserializedMessage);
+    }
+}
+
+public record MyMessage(int Value, string Text);
