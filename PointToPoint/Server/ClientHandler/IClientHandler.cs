c51dc7830b3679adcb40cc3cac2d487db0965c89
Pontus Markström <pontus.markstrom@gmail.com>
2024-11-16 10:17:30 +0100

    Update docs

--- PointToPoint/Server/ClientHandler/IClientHandler.cs
+++ PointToPoint/Server/ClientHandler/IClientHandler.cs
@@ -6,12 +6,12 @@ namespace PointToPoint.Server.ClientHandler
     /// <summary>
     /// The interface for server side specific data per client connection
     /// </summary>
-    /// The Dispose method can be used to perform cleanup when a client is disconnected
     public interface IClientHandler
     {
         /// <summary>
         /// Called when messenger is ready
         /// </summary>
+        /// This method executes on a messenger internal thread.
         /// <param name="messageSender">This instance can be used to send messages</param>
         /// <param name="messageRouter">The message router</param>
         void Init(IMessageSender messageSender, IMessageRouter messageRouter);
@@ -19,6 +19,7 @@ namespace PointToPoint.Server.ClientHandler
         /// <summary>
         /// Called when the client has disconnected
         /// </summary>
+        /// This method executes on a messenger internal thread.
         /// <param name="e">The exception that caused the disconnect</param>
         void Exit(Exception? e);
 
