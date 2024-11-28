a28d36dd0f2e0964ec2771f31d9c0ef70f8f82ed
Pontus Markström <pontus.markstrom@gmail.com>
2024-11-28 23:15:36 +0100

    Re-use code between payload serializers

---
+++ PointToPoint/Payload/AbstractTextPayloadSerializer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using PointToPointProtocol;
+
+namespace PointToPoint.Payload
+{
+    public abstract class AbstractTextPayloadSerializer : IPayloadSerializer
+    {
+        private static readonly Encoding PayloadTextEncoding = Encoding.Unicode;
+        private const string IdPayloadSeparator = " ";
+
+        private readonly Assembly[] messagesAssemblies;
+
+        protected AbstractTextPayloadSerializer(Assembly messagesAssembly)
+        {
+            messagesAssemblies = new[] { messagesAssembly, typeof(KeepAlive).Assembly };
+        }
+
+        public byte[] MessageToPayload(object message)
+        {
+            var messageType = message.GetType();
+            if (!TypeIsProtocolMessage(messageType))
+            {
+                throw new ArgumentException($"Non protocol message can not be sent: {messageType}");
+            }
+            var json = SerializeObject(message);
+            var messageString = $"{messageType}{IdPayloadSeparator}{json}";
+            return SerializeString(messageString);
+        }
+
+        protected abstract string SerializeObject(object message);
+
+        public object PayloadToMessage(byte[] payloadBytes, int length)
+        {
+            var payload = DeserializeString(payloadBytes, length);
+
+            var separatorIndex = payload.IndexOf(IdPayloadSeparator);
+            if (separatorIndex < 1)
+            {
+                throw new PayloadDeserializeException("Message type/json separator not found");
+            }
+
+            var jsonTypeString = payload.Substring(0, separatorIndex);
+            var jsonType = GetMessageType(jsonTypeString);
+            if (jsonType == null)
+            {
+                throw new PayloadDeserializeException($"Unknown message type: {jsonTypeString}");
+            }
+
+            var json = payload.Substring(separatorIndex + 1);
+
+            var message = DeserializeObject(json, jsonType);
+            if (message is null)
+            {
+                throw new PayloadDeserializeException($"JsonConvert.DeserializeObject() returned null for type {jsonTypeString}");
+            }
+            return message;
+        }
+
+        protected abstract object? DeserializeObject(string text, Type type);
+
+        private Type? GetMessageType(string typeString)
+        {
+            foreach (var assembly in messagesAssemblies)
+            {
+                var type = assembly.GetType(typeString);
+                if (type is not null)
+                {
+                    return type;
+                }
+            }
+            return null;
+        }
+
+        private bool TypeIsProtocolMessage(Type type)
+        {
+            return messagesAssemblies.Contains(type.Assembly);
+        }
+
+        private static byte[] SerializeString(string data)
+        {
+            return PayloadTextEncoding.GetBytes(data);
+        }
+
+        private string DeserializeString(byte[] bytes, int length)
+        {
+            return PayloadTextEncoding.GetString(bytes, 0, length);
+        }
+    }
+}
--- PointToPoint/Payload/YamlPayloadSerializer.cs
+++ PointToPoint/Payload/YamlPayloadSerializer.cs
@@ -1,99 +1,31 @@
 ﻿using System;
 using System.Reflection;
-using System.Text;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
-using PointToPointProtocol;
-using System.Linq;
 
 namespace PointToPoint.Payload
 {
     // This class requires that payload classes have parameterless constructors
-    public class YamlPayloadSerializer : IPayloadSerializer
+    public class YamlPayloadSerializer : AbstractTextPayloadSerializer
     {
-        private static readonly Encoding PayloadTextEncoding = Encoding.Unicode;
-        private const string IdPayloadSeparator = " ";
-
-        private readonly Assembly[] messagesAssemblies;
-
         private readonly ISerializer serializer;
         private readonly IDeserializer deserializer;
 
-        public YamlPayloadSerializer(Assembly messagesAssembly)
+        public YamlPayloadSerializer(Assembly messagesAssembly) : base(messagesAssembly)
         {
-            messagesAssemblies = new[] { messagesAssembly, typeof(KeepAlive).Assembly };
-
             var namingConvention = CamelCaseNamingConvention.Instance;
             serializer = new SerializerBuilder().WithNamingConvention(namingConvention).Build();
             deserializer = new DeserializerBuilder().WithNamingConvention(namingConvention).Build();
         }
 
-        public byte[] MessageToPayload(object message)
+        protected override string SerializeObject(object message)
         {
-            var messageType = message.GetType();
-            if (!TypeIsProtocolMessage(messageType))
-            {
-                throw new ArgumentException($"Non protocol message can not be sent: {messageType}");
-            }
-
-            var yaml = serializer.Serialize(message);
-            var messageString = $"{messageType}{IdPayloadSeparator}{yaml}";
-            return SerializeString(messageString);
+            return serializer.Serialize(message);
         }
 
-        public object PayloadToMessage(byte[] payloadBytes, int length)
+        protected override object? DeserializeObject(string text, Type type)
         {
-            var payload = DeserializeString(payloadBytes, length);
-
-            var separatorIndex = payload.IndexOf(IdPayloadSeparator);
-            if (separatorIndex < 1)
-            {
-                throw new PayloadDeserializeException("Message type/json separator not found");
-            }
-
-            var typeString = payload.Substring(0, separatorIndex);
-            var type = GetMessageType(typeString);
-            if (type == null)
-            {
-                throw new PayloadDeserializeException($"Unknown message type: {typeString}");
-            }
-
-            var yaml = payload.Substring(separatorIndex + 1);
-
-            var message = deserializer.Deserialize(yaml, type);
-            if (message is null)
-            {
-                throw new PayloadDeserializeException($"Deserialize() returned null for type {typeString}");
-            }
-            return message;
-        }
-
-        private Type? GetMessageType(string typeString)
-        {
-            foreach (var assembly in messagesAssemblies)
-            {
-                var type = assembly.GetType(typeString);
-                if (type is not null)
-                {
-                    return type;
-                }
-            }
-            return null;
-        }
-
-        private bool TypeIsProtocolMessage(Type type)
-        {
-            return messagesAssemblies.Contains(type.Assembly);
-        }
-
-        private static byte[] SerializeString(string data)
-        {
-            return PayloadTextEncoding.GetBytes(data);
-        }
-
-        private string DeserializeString(byte[] bytes, int length)
-        {
-            return PayloadTextEncoding.GetString(bytes, 0, length);
+            return deserializer.Deserialize(text, type);
         }
     }
 }
